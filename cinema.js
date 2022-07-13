fa024c05fce071172c1b1ac35c21ea79ac018c57
matheus <matheusribeiro705@gmail.com>
2022-07-12 21:04:58 -0300

    commit cine house

---
+++ cinema.js
@@ -0,0 +1,37 @@
+//variaveis arrays com atributos do filme.
+
+let cinema = "Nome da Loja";
+      console.log(cinema);
+
+let catalogo = [
+    01, 
+    "velozes e furiosos", 
+    108,
+    "Vin Diesel",
+    2022,
+    true
+];
+      console.log(catalogo);
+
+let novoCatalogo = [
+    02,
+    "vingadores", 
+    125, 
+    "viuva negra", 
+    2020, 
+    false
+];   
+       console.log(novoCatalogo); 
+
+//função para adicionar um filme ao array.
+
+catalogo.push("minons");
+      console.log(catalogo);
+
+//função buscar filme
+
+console.log(catalogo[1]);   
+
+
+
+
